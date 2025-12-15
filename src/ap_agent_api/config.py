cba48a6b4531da289e102d176f30636f8597d6dc
d.shrestha <d.shrestha@primevision.com>
2025-12-15 03:30:29 +0100

    Changed the directory

--- src/ap_agent_api/config.py
+++ src/ap_agent_api/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 # Paths
-BASE_DIR = Path(__file__).parent.parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent
 PROPERTY_RESULTS_DIR = BASE_DIR / "property_results"
 
 load_dotenv(dotenv_path=os.path.join(os.path.abspath(BASE_DIR), ".env"))
